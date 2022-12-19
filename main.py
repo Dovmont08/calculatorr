3c479fe5cc790f5fe1cf6e59de04f1f778601ed4
Maxim <maximaidary@gmail.com>
2022-12-19 21:40:36 +0300

    calculator

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 
+waht = input("Выбирете действие? (+,-,*,/,%,**): ")
+
+a = float( input("Введите первое число:"))
+b = float(input("Введите второе число"))
+
+if waht == "+":
+    c = a + b
+    print("Результат: ", str(c))
+elif  waht == "-":
+    c = a - b
+    print("Результат: " + str(c))
+elif waht == "*":
+    c = a * b
+    print("Результат: " + str(c))
+elif waht == "/":
+    c = a / b
+    print("Результат: " + str(c))
+elif waht == "%":
+    c = a % b
+    print("Результат: " + str(c))
+elif waht == "**":
+    c = a ** b
+    print("Результат: " + str(c))
+else:
+    print("неверная операция")
